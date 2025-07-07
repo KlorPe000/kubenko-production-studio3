798233a2892f846b814171d6782f7246998c8c2c
KlorPe <z76196702@gmail.com>
2025-07-07 12:04:21 +0300

    Update home.tsx

--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -1846,7 +1846,7 @@ export default function Home() {
       
       {/* Video Modal */}
       <Dialog open={isVideoModalOpen} onOpenChange={closeVideoModal}>
-        <DialogContent className="max-w-[95vw] max-h-[95vh] w-full h-auto bg-transparent border-none p-0 m-0">
+        <DialogContent className="max-w-[95vw] md:max-w-[80vw] lg:max-w-[1200px] xl:max-w-[1400px] max-h-[95vh] w-full h-auto bg-transparent border-none p-0 m-0">
           <DialogHeader className="sr-only">
             <DialogTitle>Перегляд відео</DialogTitle>
             <DialogDescription>Повноекранний перегляд відео з портфоліо</DialogDescription>
